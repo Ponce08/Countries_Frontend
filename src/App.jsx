2dde84e8b1edef9dd3b9e6b8abe13efc6a93aac3
ponce08 <yonathanponce2019@gmail.com>
2024-05-09 15:41:48 -0500

    axios 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import Welcome from './components/welcome/Welcome';
 import Home from './components/home/Home';
 import Detail from './components/detail/Detail';
 import Form from './components/form/Form';
-import axios from 'axios';
-
-// axios.defaults.baseURL = 'http://localhost:3001'
-axios.defaults.baseURL = 'https://countriesback-production-2944.up.railway.app/';
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './ridux/store.js'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './ridux/store.js';
+import App from './App.jsx';
+import axios from 'axios';
 
+// axios.defaults.baseURL = 'http://localhost:3001'
+axios.defaults.baseURL = 'https://countriesback-production-2944.up.railway.app/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
   </Provider>
-)
+);
