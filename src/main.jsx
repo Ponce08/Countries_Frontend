e0178dcc4d73ab3fe4e0bf2b10fc11ad9f3fc2b5
ponce08 <yonathanponce2019@gmail.com>
2024-05-10 11:48:42 -0500

    update vercel

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import store from './ridux/store.js';
 import App from './App.jsx';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'http://localhost:3001'
-axios.defaults.baseURL = 'https://countriesback-production-2944.up.railway.app/';
+axios.defaults.baseURL = 'http://localhost:3001'
+// axios.defaults.baseURL = 'https://countriesback-production-2944.up.railway.app/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
