13581b8806b89926b31525432efabac50c47ef9f
ponce08 <yonathanponce2019@gmail.com>
2024-06-05 19:04:14 -0500

    utils update

--- src/ridux/utils/utils.js
+++ src/ridux/utils/utils.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const funcionOrderAndFilterCountry = async(value)=>{
     
     try {
-        const { data } = await axios.get(`http://localhost:3001/countries`);
+        const { data } = await axios.get(`https://countriesback-production-83ab.up.railway.app/`);
 
         if(value === "De la A-Z"){
             const countriesOrdered_AZ = data.sort((a, b)=>{ 
